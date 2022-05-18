ac2a1dc4c4b1ae67a31a3dd335cea8321e291c04
Nitesh9609 <sharmanitesh920@gmail.com>
2022-05-18 13:22:29 +0530

    commit3

--- src/App.js
+++ src/App.js
@@ -10,24 +10,31 @@ import AllEmployee from './pages/viewAllEmployee/AllEmployee';
 import EditEmployee from './pages/editEmployee/editEmployee';
 import DisplayEmployee from './pages/displayEmployee/DisplayEmployee';
 import AddEmployee from './pages/addNewEmployee/AddEmployee';
+import Header from './pages/header/Header';
 
 function App() {
   return (
-    <div className="Apps">
-      <Routes>
-        <Route path='/' element={<HomePage/>}/>
-        <Route path='/employee' element={<EmployeeSignIn/>}/>
-        <Route path='/employeeSignUp' element={<EmployeeSignUp/>}/>
-        <Route path='/admin' element={<AdminSignIn/>}/>
-        <Route path='/adminSignUp' element={<AdminSignUp/>}/>
-        <Route path='/empDetail' element={<EmpDetail/>}/>
-        <Route path='/allEmployee' element={<AllEmployee/>}/>
-        <Route path='/updateEmployee/:id' element={<EditEmployee/>}/>
-        <Route path='/displayEmployee' element={<DisplayEmployee/>}/>
-        <Route path='/addEmployee' element={<AddEmployee/>}/>
-      </Routes>
+    <>
+      <div className='header'>
+        <Header/>
+      </div>
       
-    </div>
+      <div className="Apps">
+        <Routes>
+          <Route path='/' element={<HomePage/>}/>
+          <Route path='/employee' element={<EmployeeSignIn/>}/>
+          <Route path='/employeeSignUp' element={<EmployeeSignUp/>}/>
+          <Route path='/admin' element={<AdminSignIn/>}/>
+          <Route path='/adminSignUp' element={<AdminSignUp/>}/>
+          <Route path='/empDetail' element={<EmpDetail/>}/>
+          <Route path='/allEmployee' element={<AllEmployee/>}/>
+          <Route path='/updateEmployee/:id' element={<EditEmployee/>}/>
+          <Route path='/displayEmployee' element={<DisplayEmployee/>}/>
+          <Route path='/addEmployee' element={<AddEmployee/>}/>
+        </Routes>
+      
+      </div>
+    </> 
   );
 }
 
